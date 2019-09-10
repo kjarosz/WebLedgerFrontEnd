4a9c930341919833274e8b35460683675b14f16f
Kamil Jarosz <kjarosz19946@gmail.com>
2019-09-10 18:52:33 -0400

    Allow setting allocation center accounts.

--- src/app/components/allocation-center/allocation-center.component.ts
+++ src/app/components/allocation-center/allocation-center.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AllocationCenter } from '../../model/allocation-center';
+import { Account } from '../../model/account';
+import { AccountService } from 'src/app/services/account/account.service';
 
 @Component({
   selector: 'app-allocation-center',
@@ -9,11 +11,35 @@ import { AllocationCenter } from '../../model/allocation-center';
 export class AllocationCenterComponent implements OnInit {
 
   allocationCenter: AllocationCenter;
+  accounts: Account[];
 
-  constructor() { }
+  selectorShowing: boolean;
+
+  constructor(
+    private accountService: AccountService
+  ) { }
 
   ngOnInit() {
     this.allocationCenter = new AllocationCenter();
   }
 
+  showAccountSelector() {
+    this.loadAccountList();
+    this.selectorShowing = true;
+  }
+
+  private loadAccountList() {
+    this.accountService.getAccounts().subscribe((accounts: Account[]) => {
+      this.accounts = accounts;
+    })
+  }
+
+  hideAccountSelector() {
+    this.selectorShowing = false;
+  }
+
+  selectAccount(account: Account) {
+    this.allocationCenter.account = account;
+    this.hideAccountSelector();
+  }
 }
