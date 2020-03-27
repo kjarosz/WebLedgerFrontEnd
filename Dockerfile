2739aa48073d4298942fdfcb0f78078bb55a7eda
Kamil Jarosz <kjarosz19946@gmail.com>
2020-03-27 14:25:06 -0400

    Replicate simple node Dockerfile from Docker online docs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM node:10.16 AS build
-WORKDIR /home/kamil/WebLedgerFrontend
-COPY package.json ./
+FROM node:current-slim
+WORKDIR /usr/src/app
+COPY package.json .
 RUN npm install
+EXPOSE 4200
 COPY . .
-RUN npm run build
+CMD [ "npm", "start" ]
--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 docker build -t kjarosz/webledgerfrontend .
-docker run \
-  --name kjarosz/webledgerfrontend \
-  -p 4200:4200 \
-  --rm \
-  -d \
-  kjarosz/webledgerfrontend
+docker run --name kjarosz/webledgerfrontend -p 4200:4200 --rm -d kjarosz/webledgerfrontend
