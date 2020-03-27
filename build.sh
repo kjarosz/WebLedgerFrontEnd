3157b8d0eb01b2c7113afdd1a1e82fcbc9f03762
Kamil Jarosz <kjarosz19946@gmail.com>
2020-03-27 15:38:52 -0400

    Correct docker incantations

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t kjarosz/webledgerfrontend .
-docker run --name webledgerfrontend --rm -d kjarosz/webledgerfrontend
+docker run --name webledgerfrontend --network=webledger -p 4200:4200 --rm -d kjarosz/webledgerfrontend
